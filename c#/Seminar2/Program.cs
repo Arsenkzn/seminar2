9be7efcbfe106b877ad4abf561d4165f37058e80
«Arsen» <rchi1504@gmail.com>
2022-04-14 11:50:50 +0300

    на кратность чисел

---
+++ c#/Seminar2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+//принимает 2 числа и выводит является ли число 1 кратно второму числу. Если не кратно числу 2 то вывод остатка от деления
+// Console.Write("Введите число 1: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число 2: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a % b == 0)
+//     Console.WriteLine("Число кратно");
+// else
+//     Console.WriteLine("Число не кратно " + "Остаток = " + (a % b));
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if ((a % 7 == 0) && (a % 23 == 0))
+    Console.WriteLine("Число кратно");
+else
+    Console.WriteLine("Число не кратно ");
+
