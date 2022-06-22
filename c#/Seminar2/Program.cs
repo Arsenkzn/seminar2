772885b1955c3be9de5241917140f6c7000b326f
«Arsen» <rchi1504@gmail.com>
2022-06-22 23:20:30 +0300

    block shema

--- c#/Seminar2/Program.cs
+++ c#/Seminar2/Program.cs
@@ -10,11 +10,26 @@
 // else
 //     Console.WriteLine("Число не кратно " + "Остаток = " + (a % b));
 
-Console.Write("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
 
-if ((a % 7 == 0) && (a % 23 == 0))
-    Console.WriteLine("Число кратно");
-else
-    Console.WriteLine("Число не кратно ");
+// if ((a % 7 == 0) && (a % 23 == 0))
+//     Console.WriteLine("Число кратно");
+// else
+//     Console.WriteLine("Число не кратно ");
 
+// // int n = new Random().Next(100, 1000);
+
+// // int digit1 = n % 10;
+// // int digit3 = n / 100;
+// // int newN = digit3 * 10 + digit1;
+
+// // Console.WriteLine(newN);
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int digit1 = n % 10;
+// int digit3 = n / 100;
+// int newN = digit3*10  + digit1;
+
+// Console.WriteLine(newN);
